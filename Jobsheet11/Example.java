05622a20634d95df233ae36b9bf24761d5c3e816
Lyrafaiqahh <lyrafaiqah@gmail.com>
2023-11-08 13:37:42 +0700

    :fire: New Project Jobsheet 11

---
+++ Jobsheet11/Example.java
@@ -0,0 +1,14 @@
+package Jobsheet11;
+public class Example {
+        public static void main(String[] args) {
+         int tinggi = 10;
+       
+         for (int i = 1; i <= tinggi; ++i) {
+          for (int j = 1; j <= i; ++j) {
+           System.out.print("* ");
+          }
+          System.out.println();
+         }
+        }
+       }
+
